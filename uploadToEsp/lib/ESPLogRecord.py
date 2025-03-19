a302afce3b3aa5232881daf3f5ddd09e6d62a48b
John Gouk <johngouk@googlemail.com>
2025-03-19 16:05:34 +0000

    Fixed ESPLogRecord
    Now doesn't fail when ESP restarted without wifi and time.time() returns almost 0!

--- uploadToEsp/lib/ESPLogRecord.py
+++ uploadToEsp/lib/ESPLogRecord.py
@@ -12,16 +12,24 @@ from logging import LogRecord
 class ESPLogRecord(LogRecord):
     def set(self, name, level, message):
         super().set(name, level, message)
+        # Modified to account for not having a network connection!
+        self.ct = time.time()
+        strCt = str(self.ct)
         ct_ns = time.time_ns()
-        stringCtns = str(ct_ns)
-        self.ct = int(stringCtns[0:9])
-        self.msecs = int(stringCtns[9:15])
+        strNs = str(ct_ns)
+        # time_ns is  ssssmmmuuu000 with microSec resolution in fact
+        #             0123456789ABCD
+        #                 ^^^         want these 3 for ms
+        #print(strCt, strNs)
+        msOffset = len(strNs)-9
+        self.msecs = int(strNs[msOffset:msOffset+3])
+        #print(strNs, self.msecs) 
 
 if __name__ == "__main__":
     import logging
     logging.basicConfig(
         level=logging.INFO,
-        format='%(asctime)s.%(msecs)06d %(levelname)s - %(name)s - %(message)s')
+        format='%(asctime)s.%(msecs)03d %(levelname)s - %(name)s - %(message)s')
 
     logger = logging.getLogger(__name__)
     print("Logger before:", logger)
@@ -39,4 +47,5 @@ if __name__ == "__main__":
         now = time.ticks_ms()
         logger.info("%02d %05d Compare the msec times", i, time.ticks_diff(now, was))
         was = now
+        time.sleep(1)
         
